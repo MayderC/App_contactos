7386e5f080393df09feca0902dac47d7a5d11e61
MayderC <maydercortes1@gmail.com>
2021-01-17 14:18:23 -0600

    procedimientos almacenados

--- data_base_app_contactos.sql
+++ data_base_app_contactos.sql
@@ -18,11 +18,11 @@ CREATE TABLE `contactos` (
   `telefono` varchar(25) DEFAULT NULL,
   `cantidad` int(11) DEFAULT 0,
   `limite_cantidad` int(11) DEFAULT 15,
-  `fecha_creacion` datetime DEFAULT NULL,
+  `fecha_creacion` datetime DEFAULT current_timestamp(),
   PRIMARY KEY (`id_contacto`),
   KEY `fk_usuario_key` (`id_usuario`),
   CONSTRAINT `fk_usuario_key` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 
 DROP TABLE IF EXISTS `usuarios`;
@@ -35,10 +35,21 @@ CREATE TABLE `usuarios` (
   `tipo_usuario` varchar(20) DEFAULT 'Usuario',
   PRIMARY KEY (`id_usuario`),
   UNIQUE KEY `email_usuario` (`email_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 
 
 CREATE OR REPLACE VIEW `v_lista_usuarios` AS (select `usuarios`.`id_usuario` AS `id_usuario`,`usuarios`.`email_usuario` AS `email_usuario`,`usuarios`.`nombre_usuario` AS `nombre_usuario`,`usuarios`.`contrasena` AS `contrasena`,`usuarios`.`fecha_creacion` AS `fecha_creacion`,`usuarios`.`tipo_usuario` AS `tipo_usuario` from `usuarios`);
+
+DROP PROCEDURE IF EXISTS sp_ingresar_contacto;
+DELIMITER ;;
+CREATE PROCEDURE `sp_ingresar_contacto`(in p_id int, IN p_nombre_contacto varchar(30), in p_email varchar(30), in p_telefono varchar(30))
+BEGIN
+	
+    INSERT INTO contactos (id_usuario, nombre_contacto, email, telefono) values (p_id, p_nombre_contacto, p_email, p_telefono);
+
+END;;
+DELIMITER ;
+
 
 DROP PROCEDURE IF EXISTS sp_ingresar_usuario;
 DELIMITER ;;
@@ -48,6 +59,15 @@ BEGIN
     INSERT INTO usuarios (email_usuario, nombre_usuario, contrasena) 
     VALUES (p_email_usuario, p_nombre_usuario, p_contrasena);
 
+END;;
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS sp_listar_contactos;
+DELIMITER ;;
+CREATE PROCEDURE `sp_listar_contactos`(in p_id int)
+BEGIN
+	Select * from contactos where id_usuario = p_id;
 END;;
 DELIMITER ;
 
